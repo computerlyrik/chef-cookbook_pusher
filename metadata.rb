14bf52a28bf32a28d1d853b2abf0126a0da38606
Christian Fischer <sw-dev@computerlyrik.de>
2014-06-19 22:24:41 +0200

    release 0.4

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'Apache 2.0'
 description      'Automagically pushes your public cookbooks
                   from github to opscode community'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 depends           'git'
