84766b9a958d43f4c6b21ad9646bf9b478d7f839
Christian Fischer <sw-dev@computerlyrik.de>
2015-12-31 13:38:44 +0100

    add source url bump version

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,7 @@ license          'Apache 2.0'
 description      'Automagically pushes your public cookbooks
                   from github to opscode community'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
+
+source_url 'https://github.com/computerlyrik/chef-cookbook_pusher'
 depends           'git'
